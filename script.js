dabc1c658b4cfd8cdcfb70807b9b570b933c51d1
aline <soarescordeiroaline@gmail.com>
2026-04-22 00:32:54 -0300

    :tada initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html =
+    document.documentElement /* sempre que eu colocar um ponto (.), estou acessando propriedades e funcionalidades!!! */
+
+  html.classList.toggle("light")
+  /* toggle alterna entre dois estados (ligado/desligado, mostrar/esconder, true/false). */
+
+  // img.setAttribute => definir um atributo do elemento img
+  //querySelector() é um método que busca um elemento na página.
+
+  // pegar a tag img
+  // substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-aline-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.aline.png")
+  }
+}
