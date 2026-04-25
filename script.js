5cc84612ed96e6c7b422ca771ea93c1e411f1d85
aline <soarescordeiroaline@gmail.com>
2026-04-25 19:29:09 -0300

    docs: organize comments

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ function toggleMode() {
   html.classList.toggle("light")
   /* toggle alterna entre dois estados (ligado/desligado, mostrar/esconder, true/false). */
 
-  // img.setAttribute => definir um atributo do elemento img
-  //querySelector() é um método que busca um elemento na página.
+  const img = document.querySelector("#profile img") //querySelector() é um método que busca um elemento na página.
 
   // pegar a tag img
   // substituir a imagem
   // se tiver light mode, adicionar a imagem light
   // se tiver sem light mode, manter a imagem normal
 
-  const img = document.querySelector("#profile img")
+  // img.setAttribute => definir um atributo do elemento img
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-aline-light.png")
